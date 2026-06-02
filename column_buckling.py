66eb1e9119d6d5dce1d0f7bec4a114b8c993a9eb
benshushanorel-cyber <benshushanorel@gmail.com>
2026-06-02 15:02:12 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,13 +1,23 @@
-def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    """
-    L: אורך במ"מ
-    E: מודול אלסטיות ב-MPa
-    A: שטח חתך בממ"ר
-    r: רדיוס אינרציה במ"מ
-    c: מרחק לסיב קיצוני במ"מ
-    e: אקסצנטריות במ"מ
-    sigma_allow: מאמץ מותר ב-MPa
+import numpy as np
 
-    Return: העומס P בניוטון (float)
-    """
-    # כתבו כאן את הקוד
+def find_critical_load(L, E, A, r, c, e, sigma_allow):
+
+    def stress_difference(P):
+        angle = (L / (2 * r)) * np.sqrt(P / (E * A))
+        sigma_max = (P / A) * (1 + (e * c / r**2) * (1 / np.cos(angle)))
+        return sigma_max - sigma_allow
+
+    P_euler = (np.pi**2 * E * A * r**2) / (L**2)
+
+    low = 0.0
+    high = 0.999 * P_euler
+
+    for _ in range(100):
+        mid = (low + high) / 2
+
+        if stress_difference(mid) > 0:
+            high = mid
+        else:
+            low = mid
+
+    return (low + high) / 2
